82370ac8ceef131a2ab9cde4e034cf2ccb35fd0f
neruneruna7 <neruneruna7@gmail.com>
2025-08-27 20:01:14 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ mod data;
 mod resnet18;
 mod train;
 
+use burn::{
+    backend::{
+        Autodiff,
+        wgpu::{Metal, WgpuDevice},
+    },
+    optim::{AdamConfig, decay::WeightDecayConfig},
+};
+
+use crate::{resnet18::ResNet18Config, train::MnistTrainingConfig};
+
+pub static ARTIFACT_DIR: &str = "./tmp/burn-resnet18-mnist";
+
 fn main() {
-    println!("Hello, world!");
+    let device = WgpuDevice::default();
+    let config = MnistTrainingConfig::new(
+        ResNet18Config::new(10, 1, 64),
+        AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(5e-5))),
+    );
+    train::run::<Autodiff<Metal>>(ARTIFACT_DIR, config, device);
 }
